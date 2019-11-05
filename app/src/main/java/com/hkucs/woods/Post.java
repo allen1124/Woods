9be930f7c7cbe43ab62c59a49c7cd5450ca6447f
Allen Tsui <>
2019-11-05 16:10:37 +0800

    Add Post

---
+++ app/src/main/java/com/hkucs/woods/Post.java
@@ -0,0 +1,19 @@
+package com.hkucs.woods;
+
+
+import java.util.Date;
+
+public class Post {
+    public String pid;
+    public String author_uid;
+    public String author_username;
+    public Boolean moods;
+    public String event;
+    public String thought;
+    public String action;
+    public Date returnTime;
+
+    public Post(){}
+
+
+}
